a643999378ba60fe8b09590cf16d60bfa88754d3
unknown <exxuquanlong036@evergrandelife.com.cn>
2020-01-08 09:58:59 +0800

    删除不必要的文件

--- express/hellow/app.js
+++
@@ -1,9 +0,0 @@
-var db = require('./db');
-var sql = 'SELECT count(*) as count from user';
-db.query(sql,function(err,rows,fields){
-    if(err){
-        console.log(err);
-        return ;
-    }
-    console.log('用户数量:',rows[0].count);
-})
